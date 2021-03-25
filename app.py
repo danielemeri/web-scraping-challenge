c39926e05f09a7fdf12851ea0abedb83c24aec63
daniel <danielemeri@yahoo.com>
2021-03-24 20:40:22 -0400

    updated

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import scrape_mars
+from webdriver_manager.chrome import ChromeDriverManager
 
+#Create an instance of Flask
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
@@ -9,7 +11,7 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 # Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
 @app.route("/")
